04d679b4e9cb28fcf3df3967bc6417a6f868f11e
Abhimanyu Mittal <abhimanyumittal@D11-AbhimanyuMittal.local>
2021-06-19 18:57:49 +0530

    added file

--- Unit_2/2.03_datetime_nulls.sql
+++ Unit_2/2.03_datetime_nulls.sql
@@ -28,3 +28,31 @@ ORDER BY
 TOP/LIMIT
 OFFSET/FETCH
 */
+
+
+-- Nulls
+select isnull(card_id) from bank.card; -- 0 means not null, 1 means null
+
+-- this is used to check all the elements of a column.
+select sum(isnull(card_id)) from bank.card;
+
+select * from bank.order
+where k_symbol is null;
+
+select * from bank.order
+where k_symbol is not null and k_symbol = ' '; -- 
+
+-- to check, replace nulls
+select *, coalesce(amount, 0) from bank.order
+where amount = 1000;
+
+
+-- case/when
+select loan_id, account_id,
+case
+when status = 'A' then 'Good - Contract Finished'
+when status = 'B' then 'Defaulter - Contract Finished'
+when status = 'C' then 'Good - Contract Running'
+else 'In Debt - Contract Running'
+end as 'Status_Description'
+from bank.loan;
