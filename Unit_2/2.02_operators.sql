e2e0b92a728832d90039f69c1aed3ead5f7f6fd7
Abhimanyu Mittal <abhimanyumittal@D11-AbhimanyuMittal.local>
2021-06-19 15:31:19 +0530

    updated file

--- Unit_2/2.02_operators.sql
+++ Unit_2/2.02_operators.sql
@@ -39,3 +39,41 @@ from bank.loan;
 select duration%2
 from bank.loan;
 -- where duration%2 = 1;
+
+
+-- rounding
+select order_id, round(amount/1000,2)
+from bank.order;
+
+select floor(avg(amount)) from bank.order;
+
+select ceiling(avg(amount)) from bank.order;
+
+-- checking the number of rows in the table, both methods give the same result
+-- given that there are no nulls in the column in the second case:
+select count(*) from bank.order;
+select count(order_id) from bank.order;
+
+-- max/min
+select max(amount) from bank.order;
+select min(amount) from bank.order;
+
+-- 'string' operations
+select *, length(k_symbol) as 'Symbol_length' from bank.order;
+select *, concat('$', order_id, '-', account_id) as 'concat' from bank.order;
+
+-- formats the number to a form with commas,
+-- 2 is the number of decimal places, converts numeric to string as well
+select *, format(amount, 2) from bank.loan;
+
+select *, lower(A2), upper(A3) from bank.district;
+-- It is interesting to note that select lower(A2), upper(A3), * from bank.district; doesn't work
+
+select A2, left(A2,5), A3, ltrim(A3) from bank.district;
+-- Similar to ltrim() there is rtrim() and trim() (to remove spaces). And similar to left() there is right() (to crop the value)
+
+select *, substr(A2,3,6) from bank.district;
+
+-- position
+select A3, left(A3, position(' ' in A3)) as bla from district;
+select position('-' in 'a-b');
